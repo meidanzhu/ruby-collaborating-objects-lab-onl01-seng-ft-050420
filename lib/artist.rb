8aff2e170b07450e7cc37189ceb7a21bd95f6afb
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-18 21:32:10 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,34 +1,29 @@
 class Artist
   attr_accessor :name
   @@all = []
-  
+
   def initialize(name)
+    @name = name
+    @songs = []
 
   end
 
-  def name(name=artist)
-
-  end
-
-  def add_song
+  def add_song(song)
+    song.artist = self
 
   end
 
   def songs
-
+    @song.all.select do |song|
+      song.artist == self
+    end
   end
 
-  def self.find_or_create_by_name
-
+  def self.find_or_create_by_name(name)
+    self.all.detect {|artist| artist.name == name} || Artist.new(name).save
   end
 
   def print_songs
-
-  end
-
-
-
-
-
-
+    self.songs.each {|song| puts song.name}
+   end
 end
