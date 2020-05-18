96a726f018c85ddd194b0826315295c453062262
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-18 21:51:23 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,37 +1,42 @@
 class Artist
+
   attr_accessor :name
 
   @@all = []
 
   def initialize(name)
     @name = name
-    @songs= []
-  end
-
-  def add_song(song)
-    @songs << song
-    song.artist = self
-    song
-  end
-
-  def songs
-    @songs
-  end
-
-  def save
     @@all << self
-    self
   end
 
   def self.all
     @@all
   end
 
-  def self.find_or_create_by_name(name)
-    self.all.detect {|artist| artist.name == name} || Artist.new(name).save
+  def add_song(song)
+    song.artist = self
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.artist == self #can use binding.pry here to see what is happening
+    end
+  end
+
+  def self.find_or_create_by_name(artist_name)
+    if self.find(artist_name)
+      self.find(artist_name)
+    else
+      self.new(artist_name)
+    end
+  end
+
+  def self.find(artist_name) #helper method
+    self.all.find {|artist| artist.name == artist_name}
   end
 
   def print_songs
     self.songs.each {|song| puts song.name}
-   end
+  end
+
 end
