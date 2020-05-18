aa5372a9f358dedc6fd0da9ee40c26a901b5b481
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-18 21:18:33 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,32 @@
+class Artist
+
+  def initialize(name)
+
+  end
+
+  def name(name=artist)
+
+  end
+
+  def add_song
+
+  end
+
+  def songs
+
+  end
+
+  def self.find_or_create_by_name
+
+  end
+
+  def print_songs
+
+  end
+
+
+
+
+
+
+end
